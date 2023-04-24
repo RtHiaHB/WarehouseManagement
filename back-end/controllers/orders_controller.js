91db38f647f299db57705873ca351c4eb61cf086
Thomas Davidson <tomd1969@gmail.com>
2023-04-23 21:04:33 -0400

    Fleshed out the main routes. Left to do: search inventory by SKU.

--- back-end/controllers/orders_controller.js
+++ back-end/controllers/orders_controller.js
@@ -3,8 +3,79 @@ const db = require('../models')
 const { Orders } = db
 const { Op } = require('sequelize')
 
+//get an order by customer name, or get all orders
+// /orders?name='Thomas' will get all orders with a customer name like '%Thomas%'
+// /orders will get all orders
 orders.get('/', async (req, res) => {
-    res.status(200).json({
-        message: "Orders page"
-    })
+    try {
+        const foundOrders = await Orders.findAll({
+            where: {
+                customer_name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%`}
+            }
+        })
+        res.status(200).json(foundOrders)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
+
+//get an order by ID
+orders.get('/:id', async (req, res) => {
+    try {
+        const foundOrder = await Orders.findOne({
+            where: {
+                order_id: req.params.id
+            }
+        })
+        res.status(200).json(foundOrder)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// add an order
+orders.post('/', async (req, res) => {
+    try {
+        const newOrder = await Orders.create(req.body)
+        res.status(200).json({
+            message: 'Successfully created a new order',
+            data: newOrder
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// update an order
+orders.put('/:id', async (req, res) => {
+    try {
+        const updatedOrder = await Orders.update(req.body, {
+            where: {
+                order_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedOrder}`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//delete an order
+orders.delete('/:id', async (req, res) => {
+    try {
+        const deletedOrder = await Orders.destroy({
+            where: {
+                order_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedOrder}`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = orders
--- back-end/controllers/products_controller.js
+++ back-end/controllers/products_controller.js
@@ -4,7 +4,67 @@ const { Products } = db
 const { Op } = require('sequelize')
 
 products.get('/', async (req, res) => {
-    res.status(200).json({
-        message: "Products page"
-    })
+    try {
+        const foundProducts = await Products.findAll()
+        res.status(200).json(foundProducts)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
+
+products.get('/:id', async (req, res) => {
+    try {
+        const foundProd = await Products.findOne({
+            where: { 
+                prod_id: req.params.id
+            }
+        })
+        res.status(200).json(foundItem)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+products.post('/', async (req, res) => {
+    try {
+        const newProd = await Products.create(req.body)
+        res.status(200).json({
+            message:'Successfully created a new product',
+            data: newProd
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+products.put('/:id', async (req, res) => {
+    try {
+        const updatedProduct = await Products.update(req.body, {
+            where: {
+                prod_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedProduct} product`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+products.delete('/:id', async (req, res) => {
+    try {
+        const deletedProd = await Products.destroy({
+            where: {
+                prod_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedItems}.`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = products
