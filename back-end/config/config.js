56860e07e75d5833ea545c41e203df01fae9e3cf
tomd1969 <tomd1969@gmail.com>
2023-05-04 19:36:34 -0400

    Removed private data that should have been removed earlier.

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -1,21 +1,23 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "postgres",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
     "database": "warehouse_management",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "postgres",
-    "password": "postgres",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
     "database": "warehouse_management_test",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
-    "username": "postgres",
-    "password": "postgres",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
     "database": "warehouse_management_production",
     "host": "127.0.0.1",
     "dialect": "postgres"
